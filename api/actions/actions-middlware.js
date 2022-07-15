be4430614cc24537acc8f96cbaac6b9178996a28
Jaz Kime <JazDKime@gmail.com>
2022-07-15 12:43:07 -0700

    /api/actions GET / && /:id Complete + middleware

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,15 @@
 // add middlewares here related to actions
+const actMod = require('./actions-model')
+module.exports = {
+    async validateActId(req, res, next) {
+        const {id} = req.params;
+        let act = await actMod.get(id);
+        if(!act) return next({status: 404, message: "no action with that id!"})
+        next()
+    },
+    validateAction(req, res, next) {
+        const { description, notes } = req.body;
+        if(!description || !notes) return next({status: 400, message: "invalid"})
+        next()
+    }
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,26 @@
 // Write your "actions" router here!
+const express = require('express');
+const router = express.Router();
+
+const actMod = require('./actions-model')
+const { validateActId, validateAction } = require('./actions-middlware')
+
+router.get('/', (req, res, next) => {
+    actMod.get()
+        .then(acts => res.json(acts))
+        .catch(next)
+});
+
+router.get('/:id', validateActId, async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        let act = await actMod.get(id);
+        res.json(act);
+    } catch(err) {
+        next(err);
+    }
+});
+
+
+
+module.exports = router;
